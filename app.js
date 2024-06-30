ce76634477f5f6482045cbe61e8b87c8c0254729
SHASHWAT  GARG <gargshashwat705@gmail.com>
2024-06-30 13:30:40 +0530

    deploy commit 2

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "*",
     methods: ["GET", "PUT", "DELETE", "POST"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
