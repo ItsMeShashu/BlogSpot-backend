d576017fbcaefeea819615a558e3d9bfc457716f
SHASHWAT  GARG <gargshashwat705@gmail.com>
2024-06-30 13:41:35 +0530

    deply commits

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ dotenv.config({ path: "./config/config.env" });
 
 app.use(
   cors({
-    origin: "*",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "PUT", "DELETE", "POST"],
     credentials: true,
     allowedHeaders: ["Content-Type", "Authorization"],
